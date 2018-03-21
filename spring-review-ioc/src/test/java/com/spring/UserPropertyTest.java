d6ba316b9ec959435654cbc9647336958db28f06
Neal-Wangxd <wangxd_work@163.com>
2018-03-21 20:26:23 +0800

    使用有参构造输入属性测试

---
+++ spring-review-ioc/src/test/java/com/spring/UserPropertyTest.java
@@ -0,0 +1,28 @@
+package com.spring;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.spring.bean.User;
+
+/** 
+* @author 作者 wxd  
+* @version 1.0
+* E-mail: wangxudong@jetsen.cn
+* 创建时间：2018年3月21日 下午8:02:28 
+* 类说明  使用有参构造输入属性测试
+*/
+public class UserPropertyTest {
+	@Test
+	public void testUser(){
+		//加载spring配置文件，创建对象
+		ApplicationContext application = new ClassPathXmlApplicationContext("application.xml");
+		//得到配置文件中对象的ID
+		User user = (User) application.getBean("propertyUser");
+		System.out.println(user.hashCode());
+		user.testProperty();
+	}
+	
+}
+ 
