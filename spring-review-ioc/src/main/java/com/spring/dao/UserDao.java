d7472aae1588f17c8b4018151b0a98f5edc03893
Neal-Wangxd <wangxd_work@163.com>
2018-03-21 22:14:17 +0800

    UserDao

---
+++ spring-review-ioc/src/main/java/com/spring/dao/UserDao.java
@@ -0,0 +1,15 @@
+package com.spring.dao; 
+/** 
+* @author 作者 wxd  
+* @version 1.0
+* E-mail: wangxudong@jetsen.cn
+* 创建时间：2018年3月21日 下午8:43:31 
+* 类说明 
+*/
+public class UserDao {
+	
+	public void addUser(){
+		System.out.println("dao------add");
+	}
+}
+ 
