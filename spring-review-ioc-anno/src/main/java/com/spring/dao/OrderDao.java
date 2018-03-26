44b1aabe845cc4e2217ca65def6db1fa6803d31e
Neal-Wangxd <wangxd_work@163.com>
2018-03-26 15:12:15 +0800

    OrderDao

---
+++ spring-review-ioc-anno/src/main/java/com/spring/dao/OrderDao.java
@@ -0,0 +1,16 @@
+package com.spring.dao; 
+/** 
+* @author 作者 wxd  
+* @version 1.0
+* E-mail: wangxudong@jetsen.cn
+* 创建时间：2018年3月26日 下午2:33:22 
+* 类说明 
+*/
+public class OrderDao {
+	
+	public void buy(){
+		System.out.println("orderDao..............");
+	}
+
+}
+ 
