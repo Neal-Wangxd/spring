4785f54281d20a1e06d19fa6ab531a0a0c28e833
Neal-Wangxd <wangxd_work@163.com>
2018-03-26 15:11:57 +0800

    BookDao

---
+++ spring-review-ioc-anno/src/main/java/com/spring/dao/BookDao.java
@@ -0,0 +1,16 @@
+package com.spring.dao; 
+/** 
+* @author 作者 wxd  
+* @version 1.0
+* E-mail: wangxudong@jetsen.cn
+* 创建时间：2018年3月26日 下午2:32:29 
+* 类说明 
+*/
+public class BookDao {
+	
+	public void book(){
+		System.out.println("bookDao.............");
+	}
+
+}
+ 
