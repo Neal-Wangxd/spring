cf32c9631e580977161f1bf3295f38d260048ff0
Neal-Wangxd <wangxd_work@163.com>
2018-03-29 13:50:02 +0800

    使用注解方式实现aop --测试类

---
+++ spring-review-aop-anno/src/test/java/com/spring/AopBookTest.java
@@ -0,0 +1,28 @@
+package com.spring;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.spring.bean.Book;
+
+/** 
+* @author 作者 wxd  
+* @version 1.0
+* E-mail: wangxudong@jetsen.cn
+* 创建时间：2018年3月29日 下午3:18:33 
+* 类说明  aop测试类
+*/
+public class AopBookTest {
+
+	@Test
+	public void testUser(){
+		//加载spring配置文件，创建对象
+		ApplicationContext application = new ClassPathXmlApplicationContext("application.xml");
+		//得到配置文件中对象的ID
+		Book book = (Book) application.getBean("book");
+		System.out.println(book.hashCode());
+		book.testAop();
+	}
+}
+ 
